12f6e20b79f1f2ca40d0e614bf4315d331c94888
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2022-04-05 12:14:11 +1200

    Added boilerplate

--- daforst/App.js
+++ daforst/App.js
@@ -3,6 +3,9 @@ import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import UploadScreen from './screens/UploadScreen';
+import AssetsScreen from './screens/AssetsScreen';
+import CameraScreen from './screens/CameraScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +19,10 @@ const App = () => {
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={globalScreenOptions}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} title="DAFORST" />
+        <Stack.Screen name="Upload" component={UploadScreen} />
+        <Stack.Screen name="Assets" component={AssetsScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ daforst/screens/AssetsScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AssetsScreen = () => {
+  return (
+    <View>
+      <Text>AssetsScreen</Text>
+    </View>
+  )
+}
+
+export default AssetsScreen
