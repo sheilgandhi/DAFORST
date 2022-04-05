0137b01e7d0f9fefc0435cf70a5d72c97d14e34d
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2022-04-05 12:10:05 +1200

    Screens added

---
+++ daforst/colors.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.schemecolor.com/enchanted-forest.php
+ */
+export const COLORS = {
+  green: '#55897B',
+  cream: '#E6DAB6',
+  bronze: '#CF7D34',
+  lightBrown: '#9D5E18',
+  darkBrown: '#733F20',
+};
---
+++ daforst/screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, {useEffect} from 'react';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+
+import {COLORS} from '../colors';
+
+const image = {
+  uri: 'https://static.vecteezy.com/system/resources/previews/002/099/768/large_2x/forest-landscape-background-design-illustration-free-vector.jpg',
+};
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>DAFORST</Text>
+        <View style={styles.buttons}>
+          <View style={styles.button}></View>
+          <View style={styles.button}></View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    width: 100,
+    height: 100,
+    backgroundColor: COLORS.cream,
+    boxShadow: '0px 4px 6px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: 10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+});
+
+export default HomeScreen;
