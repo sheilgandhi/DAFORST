702b6fca3cbfa0315cadb62f14bf19d4bfc08b6f
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2022-04-22 16:19:53 +1200

    Retrieve base64 image to display

--- daforst/screens/AssetsScreen.js
+++ daforst/screens/AssetsScreen.js
@@ -1,14 +1,47 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const AssetsScreen = ({route, navigation}) => {
-  const {ipfs_uri} = route.params;
+  const {ipfs_uri} = route.params; // QmTjoh8RFwQ7AZb9ohdwm5HEfcL8iP3XaG5sHiR43n7udc
+
+  const [image, setImage] = useState('');
+
+  useEffect(() => {
+    base64ToImage();
+  }, []);
+
+  const base64ToImage = async () => {
+    try {
+      const response = await axios.get(
+        `https://ipfs.infura.io/ipfs/${ipfs_uri}`,
+      );
+      setImage(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View>
       <Text>AssetsScreen</Text>
-      <Text>{ipfs_uri}</Text>
+      <Image
+        source={{uri: `data:image/png;base64,${image}`}}
+        style={styles.image}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+});
 
 export default AssetsScreen;
