000c4c7dc72b32585ce69dac33648bedf516eb11
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2022-05-02 17:37:06 +1200

    Resolve images

--- daforst/screens/AssetsScreen.js
+++ daforst/screens/AssetsScreen.js
@@ -1,6 +1,7 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { COLORS } from '../colors';
 
 const AssetsScreen = ({ route, navigation }) => {
   // const { ipfs_uri } = route.params; // QmTjoh8RFwQ7AZb9ohdwm5HEfcL8iP3XaG5sHiR43n7udc
@@ -20,16 +21,20 @@ const AssetsScreen = ({ route, navigation }) => {
   const getAssets = async () => {
     try {
       const response = await axios.get('http://10.0.2.2:3000/api/assets');
-      setAssets(response.data);
+      const array = response.data;
+      for (let i = 0; i < array.length; i++) {
+        array[i]['imgUrl'] = await base64ToImage(array[i].ipfs_cid);
+      }
+      setAssets(array);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const base64ToImage = async asset => {
+  const base64ToImage = async ipfs_cid => {
     try {
       const response = await axios.get(
-        `https://ipfs.infura.io/ipfs/${asset.ipfs_cid}`,
+        `https://ipfs.infura.io/ipfs/${ipfs_cid}`,
       );
       return response.data;
     } catch (error) {
@@ -39,16 +44,18 @@ const AssetsScreen = ({ route, navigation }) => {
 
   return (
     <View>
-      <Text>AssetsScreen</Text>
-      {assets.map((asset, i) => (
-        <View key={i}>
-          <Text>{asset.ipfs_cid}</Text>
-          <Image
-            source={{ uri: `data:image/png;base64,${base64ToImage(asset)}` }}
-            style={styles.image}
-          />
-        </View>
-      ))}
+      {assets.length > 0 ? (
+        assets.map((asset, i) => (
+          <View key={i}>
+            <Image
+              source={{ uri: `data:image/png;base64,${asset.imgUrl}` }}
+              style={styles.image}
+            />
+          </View>
+        ))
+      ) : (
+        <ActivityIndicator size="large" color={COLORS.green} />
+      )}
     </View>
   );
 };
