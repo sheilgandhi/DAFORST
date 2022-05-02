a3c70494564a9b2313c7fbab12684e29f7eaed5a
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2022-05-02 18:35:37 +1200

    Full Assets Page Built

--- daforst/screens/AssetFullScreen.js
+++ daforst/screens/AssetFullScreen.js
@@ -1,13 +1,73 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  useWindowDimensions,
+} from 'react-native';
 import React from 'react';
+import { COLORS } from '../colors';
 
 const AssetFullScreen = ({ navigation, route }) => {
   const { asset } = route.params;
+  const { width } = useWindowDimensions();
+
   return (
-    <View>
-      <Text>{asset.name}</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: `data:image/png;base64,${asset.imgUrl}` }}
+        style={styles.image}
+      />
+      <View style={styles.information}>
+        <Text style={styles.lines}>
+          <Text style={styles.bold}>ARCore ID: </Text>
+          {asset.arcore_id}
+        </Text>
+        <Text style={styles.lines}>
+          <Text style={styles.bold}>ARCore Location: </Text>
+          {asset.arcore_location}
+        </Text>
+        <Text style={styles.lines}>
+          <Text style={styles.bold}>IPFS ID: </Text>
+          {asset.ipfs_cid}
+        </Text>
+        <Text style={styles.lines}>
+          <Text style={styles.bold}>Name: </Text>
+          {asset.name}
+        </Text>
+        <Text style={styles.lines}>
+          <Text style={styles.bold}>Owner: </Text>
+          {asset.owner}
+        </Text>
+        <Text style={styles.lines}>
+          <Text style={styles.bold}>Description: </Text>
+          {asset.description}
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default AssetFullScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  information: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 20,
+    color: COLORS.black,
+  },
+  line: {
+    marginVertical: 15,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
