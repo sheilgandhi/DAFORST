58a8ba4118fa99922bda14c217b4c20f4ec3f659
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2022-07-22 13:32:18 +1200

    ensure that cloud anchors has permissions

--- daforst/android/app/src/main/java/com/daforst/ARCore.java
+++ daforst/android/app/src/main/java/com/daforst/ARCore.java
@@ -2,6 +2,7 @@ package com.daforst;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,10 +20,15 @@ public class ARCore extends ReactContextBaseJavaModule {
     @ReactMethod
     public void startARCore() {
         // Current issue Activity is not found
-        Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.google.ar.core.examples.java.persistentcloudanchor");
-        Intent callIntent = new Intent(Intent.ACTION_VIEW, uri);
+        Uri uri = Uri.parse("com.google.ar.core.examples.java.persistentcloudanchor.CloudAnchorActivity");
+        Intent callIntent = new Intent(Intent.ACTION_SEND, uri);
+//        Intent intent = Intent.createChooser(callIntent, );
+        callIntent.setType("text/plain");
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         try {
+            Toast.makeText(getReactApplicationContext(), uri.toString(), Toast.LENGTH_LONG).show();
             getCurrentActivity().startActivity(callIntent);
+//            getCurrentActivity().startActivityForResult(takePictureIntent, 1);
         } catch (Exception e) {
             e.printStackTrace();
         }
